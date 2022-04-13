d5a18296abf669988698ee617e6413e2e1a1404f
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2022-04-13 13:54:27 +0300

    Başa Sona Ortaya Ekleme Fonksiyonları Oluşturuldu.

--- veriYapilariÖdev1/veriYapilariÖdev1.cpp
+++ veriYapilariÖdev1/veriYapilariÖdev1.cpp
@@ -4,6 +4,11 @@
 #include <iostream>
 #include <chrono>
 
+
+
+
+
+
 struct ArrayList {
     int* baslangic;     // Dizinin başlangıç adresini tutar.
     void olustur();     // Başlangıç için  gereken tanımları içerir.
@@ -13,41 +18,91 @@ struct ArrayList {
     void yazdir();      // Liste içeriğini ekrana yazdırır.
     void bosalt();      // Listenin bellek alanlarını iade eder.
     int sayac;      //Liste kaç eleman olduğunu tutar.
+    void basaEkle();    //Başa değer ekler.    
+    void ortaEkle();    //Ortaya değer ekler.
+    void sonaEkle();    //Sona değer ekler.
 };
-
-
 
 
 
 struct Node
 {
-    int deger;      //Düğümün sayılal değeri
+    int deger;      //Düğümün sayısal değeri
     Node* sonraki;  //Sonraki düğüme işaretçi
 };
 
 struct LinkedList
 {
-    Node* baslangic;    
-    void olustur();
-    void ekle(int deger, int sira);
-    void guncelle(int sira);
-    void sil(int sira);
-    void yazdir();
-    void bosalt();
-    int sayac;
+    Node* baslangic;    //Listenin başlangıç adresini tutar.
+    void olustur();     // Başlangıç için  gereken tanımları içerir.
+    void ekle(int deger, int sira);     // Değer ve Sıra numarası alarak ekleme yapar.
+    void guncelle(int sira);        //Sıra numarası alarak güncelleme yapar.
+    void sil(int sira);     //Sıra numarası alarak silme işlemi yapar.
+    void yazdir();      // Liste içeriğini ekrana yazdırır.
+    void bosalt();      // Listenin bellek alanlarını iade eder.
+    int sayac;      //Liste kaç eleman olduğunu tutar.
+    void basaEkle(Node *p);    //Başa değer ekler.    
+    void ortaEkle(Node *p);    //Ortaya değer ekler.
+    void sonaEkle(Node *p);    //Sona değer ekler.
 };
 
-typedef ArrayList VeriYapilari;
-
-VeriYapilari list;
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    list.olustur();
+    int length = 100;
 
+    LinkedList* linkL = new LinkedList();
+    std::cout << "Main Fonksiyonu Calisti\n";
+    linkL->olustur();
+    linkL->yazdir();
+
+    //for (int i = 0; i <length; i++)
+    //{
+    //    linkL->ekle(i, i);
+    //}
+    //
+    //auto begin = std::chrono::high_resolution_clock::now(); //chrono başlangıç noktası
+
+    //linkL->ekle(10, 1000);
+    //
+    //auto end = std::chrono::high_resolution_clock::now();
+    //auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+    //std::cout << "Baglantili Listede Ekleme Süresi";
+
+
+    Node* p = new Node();
+    p->deger = 0;
+    linkL->sonaEkle(p);
+    linkL->yazdir();
+
+    p = new Node();
+    p->deger = 1;
+    linkL->sonaEkle(p);
+    linkL->yazdir();
+
+    p = new Node();
+    p->deger = 3;
+    linkL->basaEkle(p);
+    linkL->yazdir();
+
+
+    p = new Node();
+    p->deger = 2;
+    linkL->sonaEkle(p);
+    linkL->yazdir();
+
+    p = new Node();
+    p->deger = 3;
+    linkL->basaEkle(p);
+    linkL->yazdir();
+    
+
+   
 }
+
+
+
 
 void ArrayList::olustur()
 {
@@ -69,6 +124,8 @@ void ArrayList::guncelle(int sira)
 
 void ArrayList::sil(int sira)
 {
+
+
 }
 
 void ArrayList::yazdir()
@@ -79,6 +136,18 @@ void ArrayList::bosalt()
 {
 }
 
+void ArrayList::basaEkle()
+{
+}
+
+void ArrayList::ortaEkle()
+{
+}
+
+void ArrayList::sonaEkle()
+{
+}
+
 
 
 
@@ -86,10 +155,17 @@ void ArrayList::bosalt()
 
 void LinkedList::olustur()
 {
+    std::cout << "LinkedList Olustur Calisti\n";
+    baslangic = NULL;
+    sayac = 0;
+
 }
+
 
 void LinkedList::ekle(int deger, int sira)
 {
+
+
 }
 
 void LinkedList::guncelle(int sira)
@@ -102,8 +178,68 @@ void LinkedList::sil(int sira)
 
 void LinkedList::yazdir()
 {
+    Node* tara;
+    tara = baslangic;
+
+    if (!tara)
+    {
+        std::cout << "Liste Bos\n";
+        return;
+    }
+    while (tara)
+    {
+        std::cout << tara->deger;
+        tara = tara->sonraki;
+    }
+    std::cout << "\n";
 }
 
 void LinkedList::bosalt()
 {
 }
+
+void LinkedList::basaEkle(Node *p)
+{
+    if (baslangic ==NULL)
+    {
+        baslangic = p;
+    }
+    else
+    {
+        p->sonraki = baslangic;
+        baslangic = p;
+    }
+}
+
+void LinkedList::ortaEkle(Node *p)
+{
+    if (baslangic ==NULL)
+    {
+        baslangic = p;
+        return;
+    }
+
+    int dugumsayisi = 0;
+    Node* tara = baslangic;
+
+}
+
+void LinkedList::sonaEkle(Node *p)
+{
+    if (baslangic ==NULL)
+    {
+        baslangic = p;
+        return;
+    }
+
+    Node* tara;
+    tara = baslangic;
+
+    while (tara->sonraki)
+    {
+        tara = tara->sonraki;
+    }
+
+    tara->sonraki = p;
+}
+
